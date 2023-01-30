6c942be332e41ee79d614920827f6e1893bb33cb
AndreiChertkov <andre.chertkov@gmail.com>
2023-01-31 01:21:11 +0300

    Add "gan" mode (GAN+ANN+TTOpt) into demo script

--- demo.py
+++ demo.py
@@ -3,17 +3,24 @@ import random
 import sys
 from time import perf_counter as tpc
 import torch
+from ttopt import TTOpt
 
 
 from am import am
 from data import Data
+from gen_gan import GenGAN
 from image import Image
 from model import Model
+from model_wrapper import ModelWrapper
 from utils import folder_ensure
+from utils import plot_image
+from utils import resize_and_pad
 from utils import sort_vector
 
 
 def run_am():
+    _time = tpc()
+
     if torch.cuda.is_available():
         device = torch.device('cuda')
     else:
@@ -40,24 +47,55 @@ def run_am():
     # image_base = Image.rand(data.sz, data.ch, 50, 180)
     image_base = image
 
-    am_time = tpc()
     image_am = am(model, image_base, iters=50)
     image_am.show('result/am.png')
-    am_time = tpc() - am_time
-    print(f'\n\n\n>>>Activation Maximization is built (time={am_time:-8.5f})')
+
+    print(f'\n\nDONE | Time: {tpc() - _time:-10.3f} sec.')
+
+
+def run_gan(evals=1.E+3, rank=4):
+    _time = tpc()
+
+    model = ModelWrapper('alexnet', device='cpu')
+    model.select_unit(('alexnet', '.classifier.Linear6', 1))
+    gen = GenGAN(name='fc8')
+    d = gen.codelen
+
+    def func(X):
+        imgs = gen.visualize_batch_np(X)
+        imgs = resize_and_pad(imgs, (227, 227), (0, 0))
+        return model.score_tsr(imgs)
+
+    def callback(last):
+        if True: # Only for debug (plot every result improvement)
+            img = gen.render(tto.x_min.reshape((1, -1)), scale=1.0)[0]
+            plot_image(img, 'result/gan_ttopt.png')
+
+    tto = TTOpt(func, d=d, a=0., b=1., p=2, q=5, evals=evals,
+        callback=callback, name='LATENT', with_cache=True, with_log=True)
+    tto.maximize(rank)
+
+    img = gen.render(tto.x_min.reshape((1, -1)), scale=1.0)[0]
+    plot_image(img, 'result/gan_ttopt.png')
+
+    print(f'\n\nDONE | Time: {tpc() - _time:-10.3f} sec.')
 
 
 if __name__ == '__main__':
-    np.random.seed(42)
-    random.seed(42)
-    torch.manual_seed(42)
+    seed = 42
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
 
     folder_ensure('result')
+    folder_ensure('result/gan')
     folder_ensure('result/logs')
 
     mode = sys.argv[1] if len(sys.argv) > 1 else 'am'
 
     if mode == 'am':
         run_am()
+    elif mode == 'gan':
+        run_gan()
     else:
         raise ValueError(f'Invalid computation mode "{mode}"')
