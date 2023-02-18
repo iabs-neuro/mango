c2039c82a0a581aeba799f413ff0c5433ad1ad17
AndreiChertkov <andre.chertkov@gmail.com>
2023-02-19 00:08:49 +0300

    Some fixes

--- opt/opt_ttopt.py
+++ opt/opt_ttopt.py
@@ -2,7 +2,7 @@ import torch
 from ttopt import TTOpt
 
 
-def opt_ttopt(model, gen, evals, rank=4):
+def opt_ttopt(model, gen, evals, rank=2):
     """Activation maximization with TTOpt."""
 
     def func(z):
@@ -12,7 +12,7 @@ def opt_ttopt(model, gen, evals, rank=4):
         a = a.detach().to('cpu').numpy()
         return a
 
-    tto = TTOpt(func, d=gen.d, a=-1., b=1., p=2, q=10, evals=evals,
+    tto = TTOpt(func, d=gen.d, a=-1., b=1., p=2, q=20, evals=evals,
         name='ttopt', with_cache=True, with_log=True)
     tto.maximize(rank)
 
