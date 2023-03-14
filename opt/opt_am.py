7dfabac31f7fb86abf1dad69be824699a6b39a31
AndreiChertkov <andre.chertkov@gmail.com>
2023-03-14 12:37:15 +0300

    More accurate AM method

--- opt/opt_am.py
+++ opt/opt_am.py
@@ -1,10 +1,15 @@
 import torch
+from time import perf_counter as tpc
 
 
 def opt_am(model, x, lr=1.E-4, evals=30, eps=1.E-7):
     """Activation Maximization (AM) method."""
+    t = tpc()
+
     x = x.detach().clone().to(model.device)
     x.requires_grad = True
+
+    a_list = []
 
     for i in range(evals):
         model.ann(x[None])
@@ -13,13 +18,21 @@ def opt_am(model, x, lr=1.E-4, evals=30, eps=1.E-7):
         G /= torch.sqrt(torch.mean(torch.mul(G, G))) + eps
         x = x + lr * G
 
-    # Check these transformations:
+        a_list.append(a.detach().to('cpu').numpy())
+
+        if i == 0 or (i+1) % 10 == 0 or i == evals-1:
+            text = f'am > '
+            text += f'm {i+1:-7.1e} | '
+            text += f't {tpc()-t:-9.3e} | '
+            text += f'y {a_list[-1]:-11.4e}'
+            text += ' <<< DONE' if i == evals-1 else ''
+            print(text)
 
     x = x.detach().cpu()
-    m = x.mean()
-    s = x.std() or 1.E-8
-    sat = 0.2
-    br = 0.8
-    x = x.sub(m).div(s).mul(sat).add(br).clamp(0., 1.)
+    # m = x.mean()
+    # s = x.std() or 1.E-8
+    # sat = 0.2
+    # br = 0.8
+    # x = x.sub(m).div(s).mul(sat).add(br).clamp(0., 1.)
 
     return x
